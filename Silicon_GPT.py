31fe8b0400ca450cf1d078d935de2737c610ed3f
umairbinmansoor <umairbinmansoor@gmail.com>
2025-01-19 13:13:22 +0500

    Update_1901025_

--- Silicon_GPT.py
+++ Silicon_GPT.py
@@ -9,8 +9,8 @@ st.markdown(
     """
     <style>
     [data-testid="stSidebar"] {
-        min-width: 200px; /* Adjust this value to control the width */
-        max-width: 200px; /* Ensures the width is fixed */
+        min-width: 250px; /* Adjust this value to control the width */
+        max-width: 300px; /* Ensures the width is fixed */
         background-color: #f4f4f4; /* Optional: Sidebar background color */
         padding-top: 20px;
     }
